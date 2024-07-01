605c41f2122cb69a365f6f76bf630466f7fa5b25
Naya <127324794+Nayaraarrais@users.noreply.github.com>
2024-07-01 08:53:01 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,29 +1,31 @@
-const btnLogin = document.querySelector("#btnLogin");
-const inputUsuario = document.querySelector("#usuario");
-const inputSenha = document.querySelector("#senha");
+const btnlogin = document.querySelector ("#btnlogin");
+const imputUuario= document.querySelector ("#usuario");
+const imputSenha = document.querySelector ("#senha");
 let usuarioLogado = null;
-
 (()=>{
-    usuarioLogado = localStorage.getItem("usuario");
-    if(usuarioLogado){
-        window.location.href = "/tmp/guest-wgo1pj/g243i2t/logado.html";
+    usuarioLogado = localStorage.getItem ("usuario");
+    if(usuarioLogado) {
+        window.location.href = "/logado.html";
+
     }
 })();
 
-btnLogin.onclick = (e) =>{
-
+btnLogin.onclick = () => {
     e.preventDefault();
 
     let usuario = inputUsuario.value;
     let senha = inputSenha.value;
-    if(usuario){
-        if(usuario === "raul"){
-            if(senha === "123"){
-                localStorage.setItem("usuario",usuario);
-                window.location.href = "/tmp/guest-wgo1pj/g243i2t/logado.html";
+    if(usuario) {
+        if(usuario ==="raul"){
+            if(senha ==="123"){  
+                localStorage.setItem("usuario", usuario);
+                window.location.helf = "/tmp/guest-qnirxf/%C3%81rea%20de%20Trabalho/g243I2t-main/index.html";
             }
-        }else{
-            inputUsuario.focus();
+            else{
+                inputUsuario.focus();
+            }      
         }
     }
-}
+    }
+   
+
